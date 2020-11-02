2c9315a13e6d31f0dcbf3451f9dafc84451b1220
markusantonwolf <wIDNQ6JMxupWnqkK>
2020-11-02 16:54:35 +0800

    Added navigation + cleaned up partials

--- src/scripts/_ta-navigation.js
+++ src/scripts/_ta-navigation.js
@@ -1,30 +1,35 @@
-// window.taNavigation = function () {
-//     return {
-//         scroll: false,
-//         default: {
-//             initiator: 'scroll',
-//         },
-//         init(options) {
-//             if (typeof options !== 'undefined') {
-//                 if (typeof options !== 'object' || options instanceof Array) {
-//                     console.warn('Options are in wrong type - should be object - default options been used')
-//                 }
-//                 for (let [key, value] of Object.entries(options)) {
-//                     this.default[key] = value
-//                 }
-//             }
-//             const scroll = document.getElementById(this.default.initiator);
-//             const scroll_offset_top = scroll.offsetTop
-//             // console.info('scroll', scroll.offsetTop);
-//             window.addEventListener('scroll', () => {
-//                 // scroll
-//                 // var position = document.documentElement.style.getPropertyValue('--navscroll')
-//                 if (window.scrollY >= scroll_offset_top) {
-//                     this.scroll = true
-//                 } else {
-//                     this.scroll = false
-//                 }
-//             })
-//         },
-//     }
-// }
+window.taNavigation = function () {
+    return {
+        scroll: false,
+        modal: false,
+        fixed: false,
+        default: {
+            initiator: 'scroll',
+        },
+        init(options) {
+            if (typeof options !== 'undefined') {
+                if (typeof options !== 'object' || options instanceof Array) {
+                    console.warn('Options are in wrong type - should be object - default options been used')
+                }
+                for (let [key, value] of Object.entries(options)) {
+                    this.default[key] = value
+                }
+            }
+            const scroll = document.getElementById(this.default.initiator)
+            const scroll_offset_top = scroll.offsetTop
+            // console.info('scroll', scroll.offsetTop);
+            window.addEventListener('scroll', () => {
+                // scroll
+                // var position = document.documentElement.style.getPropertyValue('--navscroll')
+                if (window.scrollY >= scroll_offset_top) {
+                    this.scroll = true
+                } else {
+                    this.scroll = false
+                }
+            })
+        },
+        toggleModal() {
+            this.modal = !this.modal
+        },
+    }
+}
