61e6d5383d47ff5fdb1dcd3ed20a0ca9fc139d42
markusantonwolf <wIDNQ6JMxupWnqkK>
2020-11-02 16:53:59 +0800

    New assets build

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -255,37 +255,65 @@ window.taLanguage = function () {
 //     }
 // }
 "use strict";
-// window.taNavigation = function () {
-//     return {
-//         scroll: false,
-//         default: {
-//             initiator: 'scroll',
-//         },
-//         init(options) {
-//             if (typeof options !== 'undefined') {
-//                 if (typeof options !== 'object' || options instanceof Array) {
-//                     console.warn('Options are in wrong type - should be object - default options been used')
-//                 }
-//                 for (let [key, value] of Object.entries(options)) {
-//                     this.default[key] = value
-//                 }
-//             }
-//             const scroll = document.getElementById(this.default.initiator);
-//             const scroll_offset_top = scroll.offsetTop
-//             // console.info('scroll', scroll.offsetTop);
-//             window.addEventListener('scroll', () => {
-//                 // scroll
-//                 // var position = document.documentElement.style.getPropertyValue('--navscroll')
-//                 if (window.scrollY >= scroll_offset_top) {
-//                     this.scroll = true
-//                 } else {
-//                     this.scroll = false
-//                 }
-//             })
-//         },
-//     }
-// }
 "use strict";
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+window.taNavigation = function () {
+  return {
+    scroll: false,
+    modal: false,
+    fixed: false,
+    "default": {
+      initiator: 'scroll'
+    },
+    init: function init(options) {
+      var _this = this;
+
+      if (typeof options !== 'undefined') {
+        if (_typeof(options) !== 'object' || options instanceof Array) {
+          console.warn('Options are in wrong type - should be object - default options been used');
+        }
+
+        for (var _i = 0, _Object$entries = Object.entries(options); _i < _Object$entries.length; _i++) {
+          var _Object$entries$_i = _slicedToArray(_Object$entries[_i], 2),
+              key = _Object$entries$_i[0],
+              value = _Object$entries$_i[1];
+
+          this["default"][key] = value;
+        }
+      }
+
+      var scroll = document.getElementById(this["default"].initiator);
+      var scroll_offset_top = scroll.offsetTop; // console.info('scroll', scroll.offsetTop);
+
+      window.addEventListener('scroll', function () {
+        // scroll
+        // var position = document.documentElement.style.getPropertyValue('--navscroll')
+        if (window.scrollY >= scroll_offset_top) {
+          _this.scroll = true;
+        } else {
+          _this.scroll = false;
+        }
+      });
+    },
+    toggleModal: function toggleModal() {
+      this.modal = !this.modal;
+    }
+  };
+};
 "use strict";
 
 function handleResize() {
