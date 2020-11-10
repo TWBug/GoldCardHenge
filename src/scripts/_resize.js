3f492fb02a7f6f4aea579e752c50544eade5f28a
markusantonwolf <wIDNQ6JMxupWnqkK>
2020-11-10 11:40:53 +0800

    Fixed issue with scroll navigation height + added fallback value

--- src/scripts/_resize.js
+++ src/scripts/_resize.js
@@ -1,7 +1,14 @@
 function handleResize() {
-    const navigationScrollHeight = document.getElementById('navigationScroll').offsetHeight
+    
+    // get the scroll navigations height and store it in a css custom propperty
+    var navigationScrollHeight = document.getElementById('navigationScroll').offsetHeight
+    if (navigationScrollHeight === 0) {
+        navigationScrollHeight = 58
+    }
     document.documentElement.style.setProperty('--navigationScroll', `${navigationScrollHeight}px`)
     
+    // view port height fix for mobile browsers
     const vh = window.innerHeight * 0.01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
+    
 }
