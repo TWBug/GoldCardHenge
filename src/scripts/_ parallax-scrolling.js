8ea3a7a7b23f918b7a8d6ac43c51e9f499917bd0
markusantonwolf <wIDNQ6JMxupWnqkK>
2020-11-16 13:31:30 +0800

    Cleaned

---
+++ src/scripts/_ parallax-scrolling.js
@@ -0,0 +1,8 @@
+// window.addEventListener('scroll', handleParallaxScrolling)
+
+// function handleParallaxScrolling() {
+//   const test = document.getElementById('test')
+//   test.style.top = (window.scrollY / 2) + 'px'
+//   console.info('test', test.getBoundingClientRect().top);
+//   console.info("scrolly", window.scrollY);
+// }
