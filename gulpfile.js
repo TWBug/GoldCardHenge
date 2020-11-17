2b2b2d8a3123f4eed159c3f32c324e38dadea600
markusantonwolf <wIDNQ6JMxupWnqkK>
2020-11-17 09:52:50 +0800

    Redefined build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,80 @@
-process.env.NODE_CONFIG_DIR = './.config'
+const { parallel, series, watch, src, dest } = require('gulp');
+const postcss = require('gulp-postcss');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const minify = require('gulp-minify');
+const clean = require('gulp-clean');
 
-const config = require('config')
-const { series, parallel, watch } = require('gulp')
-const { clean_all, clean_styles, clean_scripts } = require('./.gulp/clean')
-const { scripts } = require('./.gulp/scripts')
-const { styles } = require('./.gulp/styles')
+const SRC_STYLES = './src/styles/*.css';
+const SRC_SCRIPTS = './src/scripts/*.js';
+const DST_STYLES = './assets/css/';
+const DST_SCRIPTS = './assets/js/';
 
-const SOURCE_STYLES = config.get('source.styles')
-const SOURCE_SCRIPTS = config.get('source.scripts')
+const TAILWIND_CONFIG = require('./tailwind.config.js');
+TAILWIND_CONFIG.purge.enabled = false
 
-exports.build = series(clean_all, parallel(styles, scripts))
-exports.watch = function () {
-    console.info('watching: ' + SOURCE_STYLES)
-    watch(SOURCE_STYLES, series(clean_styles, styles))
-    console.info('watching: ' + SOURCE_SCRIPTS)
-    watch(SOURCE_SCRIPTS, series(clean_scripts, scripts))
-}
+const styles_develop = function styles_develop() {
+    return src(SRC_STYLES)
+        .pipe(
+            postcss([
+                require('tailwindcss')(TAILWIND_CONFIG),
+                require('autoprefixer'),
+            ])
+        )
+        .pipe(dest(DST_STYLES));
+};
+const styles_build = function styles_build() {
+    return src(SRC_STYLES)
+        .pipe(
+            postcss([
+                require('tailwindcss'),
+                require('autoprefixer'),
+                require('cssnano')({
+                    preset: 'default',
+                }),
+            ])
+        )
+        .pipe(
+            rename({
+                suffix: '.min',
+            })
+        )
+        .pipe(dest(DST_STYLES));
+};
+
+const scripts = function scripts() {
+    return src(SRC_SCRIPTS)
+        .pipe(
+            babel({
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+            })
+        )
+        .pipe(concat('scripts.js'))
+        .pipe(
+            minify({
+                ext: {
+                    min: '.min.js',
+                },
+                ignoreFiles: ['.min.js'],
+            })
+        )
+        .pipe(dest(DST_SCRIPTS));
+};
+
+const watch_files = function watch_files() {
+    console.info('watching: ' + SRC_STYLES);
+    watch(SRC_STYLES, parallel(styles_develop));
+    console.info('watching: ' + SRC_SCRIPTS);
+    watch(SRC_SCRIPTS, parallel(scripts));
+};
+
+const clean_all = function clean_all() {
+    return src(['./assets/css/**/*.css', './assets/js/**/*.js'], {
+        read: false,
+        allowEmpty: true,
+    }).pipe(clean());
+};
+
+module.exports.build = series(clean_all, parallel(styles_develop, styles_build, scripts));
+module.exports.watch = watch_files;
