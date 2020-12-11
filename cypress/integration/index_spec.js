c66927c40aecf05effa27f0d5f692bcf13401131
Ian Sinnott <ian@iansinnott.com>
2020-12-11 17:31:51 +0800

    Hide date for posts without date and add test

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -1,6 +1,7 @@
+const URL = 'http://localhost:1313';
 describe('Home page', () => {
     it('Loads and displays some text', () => {
-        cy.visit('http://localhost:1313');
+        cy.visit(URL);
         cy.contains('Taiwan');
     });
 
@@ -13,5 +14,19 @@ describe('Home page', () => {
         cy.get('[data-cy="show-search"]').click(); // Reveal search box
         cy.get('.search-box:visible').type(query).should('have.value', query);
         cy.contains('business etiquette in taiwan', { matchCase: false });
+    });
+});
+
+describe('Why Taiwan', () => {
+    it('Should not display the date on posts without a date', () => {
+        // No date!
+        cy.visit(`${URL}/en/why-taiwan/business-etiquette-in-taiwan/`);
+        cy.get('[data-cy="date-string"]').should('not.exist');
+
+        // Date please!
+        cy.visit(
+            `${URL}/en/news/an-information-platform-of-education-for-expat-children-has-been-set-up/`
+        );
+        cy.get('[data-cy="date-string"]').should('exist');
     });
 });
