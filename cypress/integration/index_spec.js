210a7327bae8fd8417632f06f99afcfd9bfd7669
Ian Sinnott <ian@iansinnott.com>
2020-12-16 13:40:11 +0800

    Remove test for missing date
    As of 2020-12-16 the missing date has been replaced with a "last modified" date.

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -16,17 +16,3 @@ describe('Home page', () => {
         cy.contains('business etiquette in taiwan', { matchCase: false });
     });
 });
-
-describe('Why Taiwan', () => {
-    it('Should not display the date on posts without a date', () => {
-        // No date!
-        cy.visit(`${URL}/en/why-taiwan/business-etiquette-in-taiwan/`);
-        cy.get('[data-cy="date-string"]').should('not.exist');
-
-        // Date please!
-        cy.visit(
-            `${URL}/en/news/an-information-platform-of-education-for-expat-children-has-been-set-up/`
-        );
-        cy.get('[data-cy="date-string"]').should('exist');
-    });
-});
