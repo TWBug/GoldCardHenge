715c9baa2bd7c440bd54b0a703caaeb5ba88fa44
markusantonwolf <wIDNQ6JMxupWnqkK>
2020-12-16 10:40:02 +0800

    Fixed scroll - jumping issue on mobile browsers

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -237,17 +237,27 @@ function handleResize() {
 
   document.documentElement.style.setProperty('--navigationScroll', "".concat(navigationScrollHeight, "px"));
   var w_aspect = Math.round(window.innerWidth / window.innerHeight * 100) / 100;
-  var home_hero = 'calc(var(--vh) * 75)';
+  var home_hero = '75vh';
 
   if (window.innerWidth < 1024) {
-    home_hero = 'calc(var(--vh) * 100)';
+    home_hero = '100vh';
   } else {
     if (w_aspect > 1.64) {
-      home_hero = 'calc(var(--vh) * 100)';
+      home_hero = '100vh';
     } else if (w_aspect < 1.2) {
-      home_hero = 'calc(var(--vh) * 50)';
+      home_hero = '50vh';
     }
-  }
+  } // var home_hero = 'calc(var(--vh) * 75)';
+  // if (window.innerWidth < 1024) {
+  //     home_hero = 'calc(var(--vh) * 100)';
+  // } else {
+  //     if (w_aspect > 1.64) {
+  //         home_hero = 'calc(var(--vh) * 100)';
+  //     } else if (w_aspect < 1.2) {
+  //         home_hero = 'calc(var(--vh) * 50)';
+  //     }
+  // }
+
 
   document.documentElement.style.setProperty('--homeHero', "".concat(home_hero));
   console.info('width x height: ', window.innerWidth + ' x ' + window.innerHeight); // view port height fix for mobile browsers
