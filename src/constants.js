86a6acd4b99ecba59ed3483cc5a4b2ca08a72e37
markusantonwolf <wIDNQ6JMxupWnqkK>
2021-01-06 17:46:58 +0800

    Fixed typo

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const DEFAULT_LANGUAGE = 'en'
-export const FALLBACK_LANGUAGE = 'dene'
+export const FALLBACK_LANGUAGE = 'en'
 export const SUPPORTED_LANGUAGES = ['en', 'zh']
 export const WELCOME_LANGUAGES = ['en', 'zh', 'de', 'es']
