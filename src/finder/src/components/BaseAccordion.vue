4cb575b02d1b8a679b3adbaecfa23dc343399eb7
markusantonwolf <wIDNQ6JMxupWnqkK>
2021-01-21 12:48:19 +0800

    Added aria controls to finder app

--- src/finder/src/components/BaseAccordion.vue
+++ src/finder/src/components/BaseAccordion.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="border-b-2 border-gray-400 border-dashed my-4 p-4 pb-8" :data-weight="item.weight">
         <button
+            :id="'accordion-button-' + id"
             type="button"
             class="w-full flex justify-between items-center text-left focus-primary px-1 -ml-1"
             @click.prevent="toggle(item.id)"
+            :aria-controls="'accordion-content-' + id"
         >
             <span class="flex-grow mr-12">
                 <span class="block text-lg font-semibold text-primary mb-1">
@@ -48,51 +50,62 @@
                 </svg>
             </span>
         </button>
-        <div class="block text-base mt-4" v-if="details">
-            <div class="flex items-center mb-8" v-if="has_info">
-                <span class="flex-shrink-0 flex-center w-3 text-orange-500">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 192 512"
-                        class="fill-current w-full"
-                        :alt="$t('exclamation')"
-                    >
-                        <title>{{ $t('exclamation') }}</title>
-                        <path
-                            d="M176 432c0 44.112-35.888 80-80 80s-80-35.888-80-80 35.888-80 80-80 80 35.888 80 80zM25.26 25.199l13.6 272C39.499 309.972 50.041 320 62.83 320h66.34c12.789 0 23.331-10.028 23.97-22.801l13.6-272C167.425 11.49 156.496 0 142.77 0H49.23C35.504 0 24.575 11.49 25.26 25.199z"
-                        />
-                    </svg>
-                </span>
-                <span class="font-medium ml-4 italic">
-                    {{ item.info[locale] }}
-                </span>
-            </div>
-            <div class="mb-4" v-if="has_provide">
-                <div class="font-bold">{{ $t('results_provide') }}</div>
-                <div class="copy" v-html="item.provide[locale]"></div>
-            </div>
-            <div class="text-sm mb-4" v-else>
-                {{ item.notes[locale] }}
-            </div>
-            <div class="flex justify-between items-center pt-4">
-                <a :href="item.link[locale]" class="flex items-center text-primary font-bold focus-primary">
-                    <span class="flex-shrink-0 w-3 mr-2">
+        <transition name="slide">
+            <div
+                :id="'accordion-content-' + id"
+                class="block text-base mt-4"
+                v-if="details"
+                :aria-expanded="details"
+                :hidden="!details"
+            >
+                <div class="flex items-center mb-8" v-if="has_info">
+                    <span class="flex-shrink-0 flex-center w-3 text-orange-500">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 256 512"
+                            viewBox="0 0 192 512"
                             class="fill-current w-full"
-                            :alt="$t('next')"
+                            :alt="$t('exclamation')"
                         >
-                            <title>{{ $t('next') }}</title>
+                            <title>{{ $t('exclamation') }}</title>
                             <path
-                                d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"
+                                d="M176 432c0 44.112-35.888 80-80 80s-80-35.888-80-80 35.888-80 80-80 80 35.888 80 80zM25.26 25.199l13.6 272C39.499 309.972 50.041 320 62.83 320h66.34c12.789 0 23.331-10.028 23.97-22.801l13.6-272C167.425 11.49 156.496 0 142.77 0H49.23C35.504 0 24.575 11.49 25.26 25.199z"
                             />
                         </svg>
                     </span>
-                    {{ $t('results_details') }} {{ $t('qualifications_' + item.ministry) }}
-                </a>
+                    <span class="font-medium ml-4 italic">
+                        {{ item.info[locale] }}
+                    </span>
+                </div>
+                <div class="mb-4" v-if="has_provide">
+                    <div class="font-bold">{{ $t('results_provide') }}</div>
+                    <div class="copy" v-html="item.provide[locale]"></div>
+                </div>
+                <div class="text-sm mb-4" v-else>
+                    {{ item.notes[locale] }}
+                </div>
+                <div class="flex justify-between items-center pt-4">
+                    <a
+                        :href="item.link[locale]"
+                        class="flex items-center text-primary font-bold focus-primary"
+                    >
+                        <span class="flex-shrink-0 w-3 mr-2">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 256 512"
+                                class="fill-current w-full"
+                                :alt="$t('next')"
+                            >
+                                <title>{{ $t('next') }}</title>
+                                <path
+                                    d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"
+                                />
+                            </svg>
+                        </span>
+                        {{ $t('results_details') }} {{ $t('qualifications_' + item.ministry) }}
+                    </a>
+                </div>
             </div>
-        </div>
+        </transition>
     </div>
 </template>
 
@@ -121,6 +134,9 @@ export default {
         locale() {
             return this.$i18n.locale;
         },
+        id() {
+            return this.item.ministry + this.item.regulation_no;
+        },
     },
     methods: {
         getQuestion(id) {
@@ -133,3 +149,16 @@ export default {
     },
 };
 </script>
+
+<style scoped>
+.slide-enter-active,
+.slide-leave-active {
+    opacity: 1;
+    transition: opacity 200ms, transform 200ms;
+    transform: translateY(0);
+}
+.slide-enter, .slide-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+    transform: translateY(-1rem);
+}
+</style>
--- src/finder/src/components/BaseQuestion.vue
+++ src/finder/src/components/BaseQuestion.vue
@@ -5,33 +5,37 @@
         :data-id="question.id"
         :data-tree="question.tree_id"
         @click.prevent="toggleActive(question.id)"
+        role="checkbox"
+        :aria-checked="question.selected"
     >
-        <span class="flex-shrink-0 h-8 w-8 mr-4" v-if="question.selected">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-                class="fill-current w-full"
-                :alt="$t('check')"
-            >
-                <title>{{ $t('check') }}</title>
-                <path
-                    d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"
-                />
-            </svg>
-        </span>
-        <span class="flex-shrink-0 h-8 w-8 mr-4" v-else>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-                class="fill-current w-full"
-                :alt="$t('circle')"
-            >
-                <title>{{ $t('circle') }}</title>
-                <path
-                    d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200z"
-                />
-            </svg>
-        </span>
+        <transition name="fade" mode="out-in">
+            <span class="flex-shrink-0 h-8 w-8 mr-4" v-if="question.selected" key="selected">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    class="fill-current w-full"
+                    :alt="$t('check')"
+                >
+                    <title>{{ $t('check') }}</title>
+                    <path
+                        d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"
+                    />
+                </svg>
+            </span>
+            <span class="flex-shrink-0 h-8 w-8 mr-4" v-else key="notselected">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    class="fill-current w-full"
+                    :alt="$t('circle')"
+                >
+                    <title>{{ $t('circle') }}</title>
+                    <path
+                        d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200z"
+                    />
+                </svg>
+            </span>
+        </transition>
         <span>
             {{ question.question_text[this.$i18n.locale] }}
         </span>
@@ -58,3 +62,18 @@ export default {
     },
 };
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+    opacity: 1;
+    transition: opacity 100ms, transform 100ms;
+    transform: translateX(0) scale(1);
+    transform-origin: center;
+}
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+    transform: translateX(0.2rem) scale(1.1);
+}
+</style>
