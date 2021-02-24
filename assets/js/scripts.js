1ff040c4c1cb1523ea3e077ad77925d8a8047f9a
markusantonwolf <wIDNQ6JMxupWnqkK>
2021-02-24 15:38:25 +0800

    Added send state + loading animation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -758,6 +758,7 @@ window.taForm = function () {
     message: '',
     form: {},
     error: {},
+    "default": {},
     init: function init(options) {
       if (typeof options !== 'undefined') {
         if (_typeof(options) !== 'object' || options instanceof Array) {
@@ -773,10 +774,12 @@ window.taForm = function () {
       for (var _property in this.$el.dataset) {
         this.form[_property] = this.$el.dataset[_property];
       } // defines error object
+      // + set default value
 
 
       for (var _property2 in this.form) {
         this.error[_property2] = false;
+        this["default"][_property2] = this.form[_property2];
       }
     },
     submit: function submit() {
@@ -788,7 +791,7 @@ window.taForm = function () {
 
       this.loading = true;
       this.$fetch('https://plain-grass-a9c0.taiwan-gold-card-office.workers.dev/', {
-        method: 'POST',
+        method: 'post',
         headers: {
           'Content-Type': 'application/json'
         },
@@ -796,6 +799,8 @@ window.taForm = function () {
       }).then(function () {
         console.info('fetch', _this);
         _this.valid = true;
+
+        _this.reset();
       })["catch"](function () {
         _this.valid = false;
       })["finally"](function () {
@@ -831,64 +836,15 @@ window.taForm = function () {
       }
 
       return true;
+    },
+    reset: function reset() {
+      // set form to default values
+      for (var property in this["default"]) {
+        this.form[property] = this.fodefaultm[property];
+      }
     }
   };
-}; // window.taForm = function () {
-//     return {
-//         language: 'en',
-//         options: {
-//             language: 'en',
-//             supported: ['en', 'zh'],
-//         },
-//         data: {
-//             authenticity_token: 'UIrYBi5cp2iO8HaN9W3v/brtWrCDstDI4LkCk6QbYX8=',
-//             'helpdesk_ticket[email]': 'test@futureward.com',
-//             'helpdesk_ticket[name]': 'Test Name',
-//             'helpdesk_ticket[subject]': 'Test Subject',
-//             'helpdesk_ticket[ticket_body_attributes][description_html]': 'Description',
-//             'meta[user_agent]':
-//                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
-//             'meta[referrer]': 'help-desk-form',
-//             utf8: '&#x2713',
-//             'helpdesk_ticket[source]': '9',
-//             retainParams: '{"widgetType":"embedded","submitTitle":"Send"}',
-//         },
-//         init(options) {
-//             if (typeof options !== 'undefined') {
-//                 if (typeof options !== 'object' || options instanceof Array) {
-//                     console.warn(
-//                         'Options are in wrong type - should be object - default options been used'
-//                     );
-//                 }
-//                 for (let [key, value] of Object.entries(options)) {
-//                     this.options[key] = value;
-//                 }
-//             }
-//             console.info(this.$refs.iframe);
-//             setTimeout(function(){
-//                 console.info('window.freshdesk', window.freshdesk.document.head);
-//             }, 3000);
-//             // Array.prototype.forEach.call(window.parent.document.querySelectorAll("link[rel=stylesheet]"), function(link) {
-//             //     var newLink = document.createElement("link");
-//             //     newLink.rel  = link.rel;
-//             //     newLink.href = link.href;
-//             //     document.head.appendChild(newLink);
-//             // });
-//             const styles = document.createElement("link");
-//             styles.rel  = 'stylesheet';
-//             styles.href = 'http://localhost:1313/css/helpdesk.css';
-//             window.freshdesk.document.head.appendChild(styles);
-//             // this.$refs.iframe.document.head.appendChild(styles);
-//             // this.$refs.iframe.contents().find("head").append($('<link rel="stylesheet" type="text/css" href="http://localhost:1313/css/helpdesk.css">')
-//             // this.$fetch({
-//             //     url: 'https://help.taiwangoldcard.tw/widgets/feedback_widget?widgetType=embedded',
-//             //     method: 'post',
-//             //     headers: { 'Content-Type': 'application/json' },
-//             //     body: JSON.stringify(this.data),
-//             // }).then(({ data }) => console.log(data));
-//         },
-//     };
-// };
+};
 "use strict";
 
 window.taImageViewer = function () {
--- src/scripts/_ta-form.js
+++ src/scripts/_ta-form.js
@@ -11,6 +11,7 @@ window.taForm = function () {
         message: '',
         form: {},
         error: {},
+        default: {},
         init(options) {
             if (typeof options !== 'undefined') {
                 if (typeof options !== 'object' || options instanceof Array) {
@@ -29,8 +30,10 @@ window.taForm = function () {
             }
 
             // defines error object
+            // + set default value
             for (const property in this.form) {
                 this.error[property] = false;
+                this.default[property] = this.form[property];
             }
         },
         submit() {
@@ -39,13 +42,14 @@ window.taForm = function () {
             }
             this.loading = true;
             this.$fetch('https://plain-grass-a9c0.taiwan-gold-card-office.workers.dev/', {
-                method: 'POST',
+                method: 'post',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(this.form),
             })
                 .then(() => {
                     console.info('fetch', this);
                     this.valid = true;
+                    this.reset();
                 })
                 .catch(() => {
                     this.valid = false;
@@ -80,68 +84,11 @@ window.taForm = function () {
             }
             return true;
         },
+        reset() {
+            // set form to default values
+            for (const property in this.default) {
+                this.form[property] = this.fodefaultm[property];
+            }
+        },
     };
 };
-
-// window.taForm = function () {
-//     return {
-//         language: 'en',
-//         options: {
-//             language: 'en',
-//             supported: ['en', 'zh'],
-//         },
-//         data: {
-//             authenticity_token: 'UIrYBi5cp2iO8HaN9W3v/brtWrCDstDI4LkCk6QbYX8=',
-//             'helpdesk_ticket[email]': 'test@futureward.com',
-//             'helpdesk_ticket[name]': 'Test Name',
-//             'helpdesk_ticket[subject]': 'Test Subject',
-//             'helpdesk_ticket[ticket_body_attributes][description_html]': 'Description',
-//             'meta[user_agent]':
-//                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
-//             'meta[referrer]': 'help-desk-form',
-//             utf8: '&#x2713',
-//             'helpdesk_ticket[source]': '9',
-//             retainParams: '{"widgetType":"embedded","submitTitle":"Send"}',
-//         },
-//         init(options) {
-//             if (typeof options !== 'undefined') {
-//                 if (typeof options !== 'object' || options instanceof Array) {
-//                     console.warn(
-//                         'Options are in wrong type - should be object - default options been used'
-//                     );
-//                 }
-//                 for (let [key, value] of Object.entries(options)) {
-//                     this.options[key] = value;
-//                 }
-//             }
-
-//             console.info(this.$refs.iframe);
-//             setTimeout(function(){
-
-//                 console.info('window.freshdesk', window.freshdesk.document.head);
-//             }, 3000);
-
-//             // Array.prototype.forEach.call(window.parent.document.querySelectorAll("link[rel=stylesheet]"), function(link) {
-//             //     var newLink = document.createElement("link");
-//             //     newLink.rel  = link.rel;
-//             //     newLink.href = link.href;
-//             //     document.head.appendChild(newLink);
-//             // });
-
-//             const styles = document.createElement("link");
-//             styles.rel  = 'stylesheet';
-//             styles.href = 'http://localhost:1313/css/helpdesk.css';
-//             window.freshdesk.document.head.appendChild(styles);
-//             // this.$refs.iframe.document.head.appendChild(styles);
-
-//             // this.$refs.iframe.contents().find("head").append($('<link rel="stylesheet" type="text/css" href="http://localhost:1313/css/helpdesk.css">')
-
-//             // this.$fetch({
-//             //     url: 'https://help.taiwangoldcard.tw/widgets/feedback_widget?widgetType=embedded',
-//             //     method: 'post',
-//             //     headers: { 'Content-Type': 'application/json' },
-//             //     body: JSON.stringify(this.data),
-//             // }).then(({ data }) => console.log(data));
-//         },
-//     };
-// };
