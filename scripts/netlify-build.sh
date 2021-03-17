c82232c7c018daefc4d00ea73527dcf895951df2
Ian Sinnott <ian@iansinnott.com>
2021-03-17 08:41:53 +0800

    Factor out netlify build script

---
+++ scripts/netlify-build.sh
@@ -0,0 +1,33 @@
+# This script is responsible for conditionally building parts of the app in our
+# netlify environment. Initially created to alleviate long build-times in the
+# front-end codebase which were unrelated to content changes.
+main() {
+    local non_content_changelist="$(git diff --name-only HEAD^ HEAD -- . ':!content/')"
+    local content_changelist="$(git diff --name-only HEAD^ HEAD -- content/)"
+
+    if [[ -n "${non_content_changelist}" ]]
+    then
+        echo "[BUILD] Detected non-content changes in the following files:"
+        for _f in $non_content_changelist; do
+            echo "        $_f"
+        done
+        echo "[BUILD] Running: npm run build:finder ..."
+        npm run build:finder
+    else
+        echo "[SKIP] No changes outside of ./content"
+    fi
+
+    if [[ -n "${content_changelist}" ]]
+    then
+        echo "[BUILD] Detected changed content in the following files:"
+        for _f in $content_changelist; do
+            echo "        $_f"
+        done
+        echo "[BUILD] npm run build:staging ..."
+        npm run build:staging
+    else
+        echo "[SKIP] No changes to ./content"
+    fi
+}
+
+main
