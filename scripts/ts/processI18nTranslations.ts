9667fd653f8058431162ac2b973b6b3daeb88c2a
Ian Sinnott <ian@iansinnott.com>
2021-03-22 15:02:09 +0800

    Automatically generate translation files from the CMS-editable file
    The netlify CMS, unfortunately, will not edit the root node of a yaml
    file. You _must_ edit a key-value dict. For this reason, the
    hugo-supported i18n file format is a no-go for CMS editing. This is a
    workaround that allows editing the translation strings from within the
    CMS.

---
+++ scripts/ts/processI18nTranslations.ts
@@ -0,0 +1,34 @@
+import assert from 'assert';
+import fs from 'fs';
+import yaml from 'js-yaml';
+import path from 'path';
+
+const DEBUG = !!process.env.DEBUG;
+interface ITranslationLangDict {
+    items: Array<{
+        id: string;
+        translation: string;
+    }>;
+}
+interface ITranslationFile {
+    en: ITranslationLangDict;
+    zh: ITranslationLangDict;
+}
+
+if (require.main === module) {
+    const filepath = path.resolve(__dirname, '../../data/translations_en_zh.yaml');
+    console.log(`[INFO] Reading translations from YAML: ${filepath}`);
+    // @ts-ignore The yaml lib seems to have poor typing.
+    const data: ITranslationFile = yaml.load(fs.readFileSync(filepath, { encoding: 'utf-8' }));
+
+    const translations = [
+        ['en', data.en.items],
+        ['zh', data.zh.items],
+    ];
+
+    for (const [lang, data] of translations) {
+        const outfile = path.join('i18n', `${lang}.yaml`);
+        fs.writeFileSync(outfile, yaml.dump(data), { encoding: 'utf8' });
+        console.log(`[INFO] Wrote ${outfile}`);
+    }
+}
