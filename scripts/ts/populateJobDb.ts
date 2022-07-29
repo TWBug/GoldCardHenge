31bc422815a102e5948b14325937a8c77a2884f9
mikehengetech <97877231+mikehengetech@users.noreply.github.com>
2022-07-29 17:04:54 +0800

    ourLocalServerTest0729dbA
    http://114.32.58.99:8088/

--- scripts/ts/populateJobDb.ts
+++ scripts/ts/populateJobDb.ts
@@ -21,7 +21,7 @@ interface IDBRowJob extends IAdapterOutput {
 }
 
 const MAPPINGS: { [k: string]: IAdapterConstructor } = {
-    'web.archive.org': CakeResumeAdapter,
+    '114.32.58.99:8088': CakeResumeAdapter,
 };
 
 const getAdapter = (url: string): IAdapter => {
