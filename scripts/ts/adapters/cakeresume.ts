880866bd97cd7b3dbf2a622e012790721974ff6f
mikehengetech <97877231+mikehengetech@users.noreply.github.com>
2022-08-02 12:31:59 +0800

    t1231
    t1231c

--- scripts/ts/adapters/cakeresume.ts
+++ scripts/ts/adapters/cakeresume.ts
@@ -39,7 +39,7 @@ export default class CakeResumeAdapter implements IAdapter {
 
     getResultsFromData(data: typeof __APP_INITIAL_REDUX_STATE__) {
         // @ts-ignore
-        const { rawResults } = data.props.pageProps.serverState.initialResults.Job.jobResultsState;
+        const { rawResults } = data.props.pageProps.serverState.initialResults.Job;
         let result;
         
         console.log(rawResults);
