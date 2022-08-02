e6efe4ebca7a2c3b0f8bbd6cc5e6930f04ce00ac
mikehengetech <97877231+mikehengetech@users.noreply.github.com>
2022-08-02 16:45:11 +0800

    L102//console.log(script)T1645
    //console.log(script);

--- scripts/ts/adapters/cakeresume.ts
+++ scripts/ts/adapters/cakeresume.ts
@@ -99,7 +99,7 @@ export default class CakeResumeAdapter implements IAdapter {
         const script = $('script').filter(
             (_, x) => !!$(x).html()?.includes('__APP_INITIAL_REDUX_STATE__')
         );
-        console.log(script);
+        //console.log(script);
         assert(script.length > 0, 'Could not locate app data script in request body. Exiting.');
 
         const raw = script.html();
