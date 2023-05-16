6cfbd2fa935f6fe16129b0f08e25a331557cd1f4
Wilson <wilson@hengetech.com.tw>
2023-05-16 15:57:40 +0800

    test

--- scripts/ts/HighLevel.ts
+++ scripts/ts/HighLevel.ts
@@ -2,25 +2,25 @@ import assert from 'assert';
 import fs from 'fs';
 
 interface Job {
-  aasmState: string;
-  contentUpdatedAt: string;
-  googlePlaces: { name: string }[];
-  jobType: string;
-  minWorkExpYear: string;
-  numberOfManagement: string;
-  numberOfOpenings: number;
-  path: string;
-  profession: string;
-  salaryCurrency: string;
-  salaryMax: number;
-  salaryMin: number;
-  salaryType: string;
-  seniorityLevel: string;
-  suspended: boolean;
-  tagsStrippedDescription: string;
-  title: string;
-  uniqueImpressionsCount: number;
-  tags: string[];
+  aasmState?: string;
+  contentUpdatedAt?: string;
+  googlePlaces?: { name: string }[];
+  jobType?: string;
+  minWorkExpYear?: string;
+  numberOfManagement?: string;
+  numberOfOpenings?: number;
+  path?: string;
+  profession?: string;
+  salaryCurrency?: string;
+  salaryMax?: number;
+  salaryMin?: number;
+  salaryType?: string;
+  seniorityLevel?: string;
+  suspended?: boolean;
+  tagsStrippedDescription?: string;
+  title?: string;
+  uniqueImpressionsCount?: number;
+  tags?: any[];
 }
 
 function adaptJob(jobData: any): Job {
@@ -32,7 +32,7 @@ function adaptJob(jobData: any): Job {
 }
 
 function parseJobData(): void {
-const script = $('#__NEXT_DATA__');
+const script = document.getElementById('__NEXT_DATA__');
 const jsonData=JSON.parse(script.textContent);
 const jobCollection = jsonData.props.pageProps.initialState.job.graphQlJobCollection;
 const jobEntities = jobCollection.entities;
