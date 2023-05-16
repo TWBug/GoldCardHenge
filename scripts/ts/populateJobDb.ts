fbe1c9a6cbb16fe6b0d189b8fe6bb769f5b19013
Wilson <wilson@hengetech.com.tw>
2023-05-16 11:43:51 +0800

    test

--- scripts/ts/populateJobDb.ts
+++ scripts/ts/populateJobDb.ts
@@ -22,7 +22,7 @@ interface IDBRowJob extends IAdapterOutput {
 }
 
 const MAPPINGS: { [k: string]: IAdapterConstructor } = {    
-    'www.cakeresume.com/companies' : CakeResumeHighLevelAdapter,       
+    'www.cakeresume.com/companies' : CakeResumeAdapter,       
 };
 
 const getAdapter = (url: string): IAdapter => {
