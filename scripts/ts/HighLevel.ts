fd6369fbbec5f72e62007c4aac8d396244359fe9
Wilson <wilson@hengetech.com.tw>
2023-05-16 15:48:04 +0800

    try new method

---
+++ scripts/ts/HighLevel.ts
@@ -0,0 +1,44 @@
+import assert from 'assert';
+import fs from 'fs';
+
+interface Job {
+  aasmState: string;
+  contentUpdatedAt: string;
+  googlePlaces: { name: string }[];
+  jobType: string;
+  minWorkExpYear: string;
+  numberOfManagement: string;
+  numberOfOpenings: number;
+  path: string;
+  profession: string;
+  salaryCurrency: string;
+  salaryMax: number;
+  salaryMin: number;
+  salaryType: string;
+  seniorityLevel: string;
+  suspended: boolean;
+  tagsStrippedDescription: string;
+  title: string;
+  uniqueImpressionsCount: number;
+  tags: string[];
+}
+
+function adaptJob(jobData: any): Job {
+  return {
+    title: jobData.title,
+    salaryMin: jobData.salaryMin,
+    salaryMax: jobData.salaryMax,
+  };
+}
+
+function parseJobData(): void {
+const script = $('#__NEXT_DATA__');
+const jsonData=JSON.parse(script.textContent);
+const jobCollection = jsonData.props.pageProps.initialState.job.graphQlJobCollection;
+const jobEntities = jobCollection.entities;
+
+const jobs: Job[] = Object.values(jobEntities).map((jobData) => adaptJob(jobData));
+
+console.log(jobs);
+  
+}
